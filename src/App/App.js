31a63d9199accc6df7364c26c2fc363ee1ef7cf9
Mhayk Whandson <hi@mhayk.com>
2021-07-27 13:19:18 +0100

    CssBaseline component has been imported to reset css.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,26 @@
+import { CssBaseline, makeStyles } from '@material-ui/core';
+import Header from '../components/Header';
 import SideMenu from '../components/SideMenu';
 import './App.css';
 
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: '320px',
+    width: '100%'
+  }
+})
+
+
 function App() {
+  const classes = useStyles();
+
   return (
     <>
       <SideMenu />
-      <div>here we go</div>
+      <div className={classes.appMain}>
+        <Header />
+      </div>
+      <CssBaseline />
     </>
   );
 }
